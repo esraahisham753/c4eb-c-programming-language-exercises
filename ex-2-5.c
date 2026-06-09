a6dcb1a1ba92be2345ffb1874a01a400382ba064
Esraa Abduallah <esraahisham753@gmail.com>
2026-06-09 18:21:25 +0300

    Feat: Write a C program to implement this Python program, using the fgets function instead of scanf.
    print('Enter line')
    line = input()
    print('Line:', line)

---
+++ ex-2-5.c
@@ -0,0 +1,16 @@
+/*
+Write a C program to implement this Python program, using the fgets function instead of scanf.
+
+print('Enter line')
+line = input()
+print('Line:', line)
+*/
+
+#include <stdio.h>
+
+int main() {
+    char line[1001];
+    printf("Enter a line:\n");
+    fgets(line, 1000, stdin);
+    printf("Line: %s", line);
+}
