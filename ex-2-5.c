cd907aa9ea2446e91c1e2b79eb19da2f463e27a2
Esraa Abduallah <esraahisham753@gmail.com>
2026-06-09 18:13:30 +0300

    Fix: loop while the scanf returns 1 and add the null terminator char.

--- ex-2-5.c
+++ ex-2-5.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
+// Write a C program to implement this Python program, using scanf.
+
 int main() {
     char line[1001];
     printf("Enter line\n");
     
     int i;
-    for (i = 0; scanf("%c", &line[i]) != 0 && i < 1000; i++)
+    for (i = 0; scanf("%c", &line[i]) == 1 && i < 1000; i++)
     ;
+
+    line[i] = '\0';
 
     printf("Line: %s", line);
 }
